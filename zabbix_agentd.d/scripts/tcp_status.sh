0ccc309abd200abb6903fe73e8ec2cf2c0dc221f
sundayle <shaopeng1995@gmail.com>
2018-06-28 11:59:10 +0800

    zabbix

--- zabbix_agentd.d/scripts/tcp_status.sh
+++ zabbix_agentd.d/scripts/tcp_status.sh
@@ -3,7 +3,7 @@
 #Date: 2018-06-17
 #Description: monitor tcp status
 LOGFILE='/tmp/tcp_status.txt'
-[ $# -ne 1 ] && echo "Usage:(CLOSE-WAIT|CLOSED|CLOSING|ESTAB|FIN-WAIT-1|FIN-WAIT-2|LAST-ACK|LISTEN|SYN-RECV SYN-SENT|TIME-WAIT)" && exit 1
+[ $# -ne 1 ] && echo "USAGE:$0 (CLOSE-WAIT|CLOSED|CLOSING|ESTAB|FIN-WAIT-1|FIN-WAIT-2|LAST-ACK|LISTEN|SYN-RECV|SYN-SENT|TIME-WAIT)" && exit 1
 
 TCP_STATUS_FUN(){
    ss -ant | awk 'NR>1 {++S[$1]} END {for (a in S) print a,S[a]}' > $LOGFILE
